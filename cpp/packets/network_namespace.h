4a1fd290f07de114b02710f533a35a86b3033b44
Trevor Assaf <trevor.assaf@gmail.com>
2015-07-25 00:04:24 -0400

    packet headers

---
+++ cpp/packets/network_namespace.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "../network_namespace.h"
+
+namespace Network {
+  template <typename Tdata> class Packet;
+  class PacketReader;
+  class PacketWriter;
+};
---
+++ cpp/packets/packet.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "network_namespace.h"
+
+#include <cstdlib>
+
+template <typename Tdata> class Network::Packet {
+
+  private:
+    const Tdata _data;
+    const size_t _dataSize;
+
+    Packet();
+    void setData(const Tdata data);
+
+  public:
+    Packet(const Tdata & data);
+    const Tdata & getData() const;
+    size_t getDataSize() const;
+};
