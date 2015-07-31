c65ff3234cf6356cf0f4b12a6383becde843d12f
Trevor Assaf <trevor.assaf@gmail.com>
2015-07-31 12:45:11 -0400

    enum keyer namespace

--- cpp/util/enum_keyer.h
+++ cpp/util/enum_keyer.h
@@ -2,8 +2,10 @@
 
 #include <cstdlib>
 
-struct Network::EnumClassKeyer {
-    template <typename T> std::size_t operator()(const T & t) const {
-      return static_cast<std::size_t>(t);
-    }
+namespace Network {
+  struct EnumClassKeyer {
+      template <typename T> std::size_t operator()(const T & t) const {
+        return static_cast<std::size_t>(t);
+      }
+  };
 };
